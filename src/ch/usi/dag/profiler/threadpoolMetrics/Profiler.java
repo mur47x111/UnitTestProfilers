0da9aa96966fa0378ae4ffb99f56524b97b4be40
Andrea Rosà <andrea.rosa@usi.ch>
2015-11-18 16:14:23 +0100

    New instrumentation

--- src/ch/usi/dag/profiler/threadpoolMetrics/Profiler.java
+++ src/ch/usi/dag/profiler/threadpoolMetrics/Profiler.java
@@ -1,8 +1,11 @@
-package ch.usi.dag.profiler.threadpool;
+package ch.usi.dag.profiler.threadpoolMetrics;
 
 import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
 import ch.usi.dag.profiler.Dumper;
@@ -13,25 +16,41 @@ public class Profiler {
 	private static final ReentrantLock globalLock = new ReentrantLock();
 
 	private static volatile String currentTest;
+	private static final Map<String, AtomicLong> allocations;
+	private static final Map<String, AtomicLong> invocations;
 	private static volatile Set<String> validated;
-	
+
 	static {
 		currentTest = null;
+		allocations = new ConcurrentHashMap<>();
+		invocations = new ConcurrentHashMap<>();
 		validated = new ConcurrentSkipListSet<>();
 		Runtime.getRuntime().addShutdownHook(new Thread(Profiler::dump));
 	}
 
 	public static void dump() {
+		dump(allocations, "allocations-");
+		dump(invocations, "invocations-");
+		dump(validated, "validated-");
+	}
+
+	public static void dump(Map<String, AtomicLong> map, String prefix) {
 		try (Dumper dumper = new FileDumper(
-				"results-validation" + java.lang.management.ManagementFactory.getRuntimeMXBean().getName())) {			
-			for (String s: validated)
-				dumper.println("THREADPOOL " + s);
-		
+				prefix + "results" + java.lang.management.ManagementFactory.getRuntimeMXBean().getName())) {
+			map.entrySet().forEach(r -> dumper.println(r.getKey() + " " + r.getValue()));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-	
+	}
+
+	public static void dump(Set<String> set, String prefix) {
+		try (Dumper dumper = new FileDumper(
+				prefix + "results" + java.lang.management.ManagementFactory.getRuntimeMXBean().getName())) {
+			for (String s: validated)
+				dumper.println("THREADPOOL " + s);			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void startTest(String m) {
@@ -40,18 +59,34 @@ public class Profiler {
 	}
 
 	public static void endTest(String m) {
+		if (!validated.contains(currentTest)) {
+			allocations.remove(currentTest);
+			invocations.remove(currentTest);
+		}			
 		currentTest = null;
 		globalLock.unlock();
 	}
 
+	public static void profileAllocation() {
+		if (globalLock.isLocked()) {
+			allocations.computeIfAbsent(currentTest, k -> new AtomicLong()).incrementAndGet();
+		}
+	}
+
+	public static void profileInvocation() {
+		if (globalLock.isLocked()) {
+			invocations.computeIfAbsent(currentTest, k -> new AtomicLong()).incrementAndGet();
+		}
+	}
+
 	public static void validate() {
-		
+
 		/* This method is called in any Object implementing a ThreadPool-related interface.
 		 * We just need to check that the global lock is locked, and that was locked by the same thread as the current one */  
-		
+
 		//System.out.println("Validate() entered! Method: " +currentTest + " locked: "+ globalLock.isLocked()+ " thread: "+ globalLock.isHeldByCurrentThread());
 		try {
-		
+
 			if (globalLock.isLocked() && globalLock.isHeldByCurrentThread()) {
 				//if (globalLock.isLocked()) {
 				// here we assume no invocation from the underlying framework
@@ -59,9 +94,10 @@ public class Profiler {
 				//System.out.println("YES!");
 			}
 		}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-	}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 
+	}
+	
 }
